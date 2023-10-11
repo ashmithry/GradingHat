6fee2e4ee2246466335283bd6c350f771fdcc438
Taha Rawjani <tahakazim08@gmail.com>
2023-10-11 17:10:15 -0400

    APi Keeps timing out

--- app/src/main/java/com/example/cac2023/MainActivity.java
+++ app/src/main/java/com/example/cac2023/MainActivity.java
@@ -17,15 +17,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cac2023.databinding.ActivityMainBinding;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     public static APICaller API;
+
+    public String essay ="Immortality. Existential threat. Humanity’s last invention. The upcoming development and rise of the artificial intelligence (AI) singularity presents an interesting situation to mankind. The singularity, or also known as artificial super intelligence, is what will happen to AI when it achieves a higher intelligence than humans. Unlike the AI of today, artificial super intelligence, or ASI, wouldn’t be dependent on humans for support, as it would easily be able to improve itself and ‘code’ itself to get faster and smarter. This would lead to an exponential increase in the intelligence of this AI, until we can’t even comprehend it. As AI continues to grow, how will it impact the economy in the new era of the singularity? According to a variety of sources, the development of ASI would lead to the end of capitalism, because AI would displace job opportunities, lead to government bankruptcy, and outperform humans.\n";
+    public String rubric = "Introduction, Thesis, & Conclusion\n" +
+                    "\n" +
+                    "\t\n" +
+                    "10\n" +
+                    "\n" +
+                    "Includes all parts of Proficient, plus…\n" +
+                    "*Hook & transition sentences (within intro) are engaging\n" +
+                    "*Presents compelling background that skillfully introduces topic\n" +
+                    "* Thesis is insightful that takes a position that is specific, arguable, and complex\n" +
+                    "* Conclusion skillfully  and offers a possible solution";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +58,13 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
         API = new APICaller(1000, "You are going to grade an essay and provide feedback based on the rubric and a teacher's leniency.");
-
+        API.requestAPI("This is my Essay:" + essay + "\nThis is my rubric:" + rubric , System.out::println);
     }
 
     @Override
