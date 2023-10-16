855397c618190f5c4b66b93b370380b7f365f558
2900xt <115376060+2900xt@users.noreply.github.com>
2023-10-16 19:58:42 -0400

    Added a graded paper fragment

---
+++ app/src/main/java/com/example/cac2023/ui/graded_paper/GradedPaperFragment.java
@@ -0,0 +1,31 @@
+package com.example.cac2023.ui.graded_paper;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.cac2023.databinding.FragmentDashboardBinding;
+
+public class GradedPaperFragment extends Fragment {
+
+    private FragmentDashboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
