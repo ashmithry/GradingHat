078c616b0fbeb31f5c15875a013a6b90141c9e13
Taha Rawjani <tahakazim08@gmail.com>
2023-10-17 09:35:54 -0400

    just added a recyclerview adapter

---
+++ app/src/main/java/com/example/cac2023/ui/papers/PaperAdapter.java
@@ -0,0 +1,69 @@
+package com.example.cac2023.ui.papers;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cac2023.R;
+
+public class PaperAdapter extends RecyclerView.Adapter<PaperAdapter.ViewHolder> {
+
+    private String[] localDataSet;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.textView);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView
+     */
+    public PaperAdapter(String[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.recycler_frame_layout, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localDataSet[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+}
